ba331ffafb9a2b87e0ed9cc9c7e218f4fb21e091
Christian Mauricio Bravo <cbravo@estratec.local>
2020-10-08 23:24:45 -0500

    Basic skech

---
+++ README.md
@@ -0,0 +1,14 @@
+# TRICKY GAME
+
+## Docker:
+
+Use docker-compose up to build the containers with the services that the game need to work.
+
+## Frontend:
+
+Vue 2
+
+
+## Back and DB:
+
+Node 12 with MongoDb
---
+++ backend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    parser: "babel-eslint",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
+      env: {
+        jest: true,
+        node: true,
+      },
+    },
+  ],
+};
